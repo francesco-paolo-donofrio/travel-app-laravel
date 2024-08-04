8db13d2e73171c72ebf055581d7fe6df8b66c8d6
Frankosviz <geom.francescopaolo@gmail.com>
2024-08-04 20:43:29 +0200

    api controller - done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TravelController;
+use App\Http\Controllers\Api\DayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/travel', [TravelController::class, 'index'])->name('travels');
+    Route::get('/travel/create', [TravelController::class, 'create'])->name('travels.create');
+    Route::get('/travel/{travel}/edit', [TravelController::class, 'edit'])->name('travels.edit');
+    Route::post('/travel', [TravelController::class, 'store'])->name('travels.store');
+    Route::put('/travel/{travel}', [TravelController::class, 'update'])->name('travels.update');
+    Route::delete('/travel/{travel}', [TravelController::class, 'destroy'])->name('travels.destroy');
+    Route::get('/day', [DayController::class, 'index'])->name('days');
+    Route::get('/day/create', [DayController::class, 'create'])->name('days.create');
+    Route::get('/day/{day}/edit', [DayController::class, 'edit'])->name('days.edit');
+    Route::post('/day', [DayController::class, 'store'])->name('days.store');
+    Route::put('/day/{day}', [DayController::class, 'update'])->name('days.update');
+    Route::delete('/day/{day}', [DayController::class, 'destroy'])->name('days.destroy');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\TravelController;
-use App\Http\Controllers\Admin\DayController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -24,18 +22,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::get('/travel', [TravelController::class, 'index'])->name('travels');
-    Route::get('/travel/create', [TravelController::class, 'create'])->name('travels.create');
-    Route::get('/travel/{travel}/edit', [TravelController::class, 'edit'])->name('travels.edit');
-    Route::post('/travel', [TravelController::class, 'store'])->name('travels.store');
-    Route::put('/travel/{travel}', [TravelController::class, 'update'])->name('travels.update');
-    Route::delete('/travel/{travel}', [TravelController::class, 'destroy'])->name('travels.destroy');
-    Route::get('/day', [DayController::class, 'index'])->name('days');
-    Route::get('/day/create', [DayController::class, 'create'])->name('days.create');
-    Route::get('/day/{day}/edit', [DayController::class, 'edit'])->name('days.edit');
-    Route::post('/day', [DayController::class, 'store'])->name('days.store');
-    Route::put('/day/{day}', [DayController::class, 'update'])->name('days.update');
-    Route::delete('/day/{day}', [DayController::class, 'destroy'])->name('days.destroy');
 });
 
 
