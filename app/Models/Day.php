cf7ef927d57eb10e29cf7cdd97c1b85321d70d19
Frankosviz <geom.francescopaolo@gmail.com>
2024-08-04 12:17:27 +0200

    routes and model

--- app/Models/Day.php
+++ app/Models/Day.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Day extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'place',
+        'description',
+        'image',
+        'food',
+        'curiosity',
+        'totalSpent',
+    ];
 }
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Travel extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'date',
+        'rating',
+    ];
 }
